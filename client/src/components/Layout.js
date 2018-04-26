adce452f0624156435ee246dea1769d1bf91a38a
KinkMustard <alec2001@gmail.com>
2018-04-25 22:38:09 -0600

    fixed endpoint

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -4,7 +4,7 @@ import { USER_CONNECTED, LOGOUT, VERIFY_USER } from "../Events";
 import LoginForm from "./LoginForm";
 import ChatContainer from "./chats/ChatContainer";
 
-const socketUrl = "http://localhost:5000";
+const socketUrl = process.env.NODE_ENV === "development" ? "http://192.168.1.97:5000" : "/";
 export default class Layout extends Component {
   constructor(props) {
     super(props);
